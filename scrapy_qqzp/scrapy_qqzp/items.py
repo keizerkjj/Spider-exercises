ca5614fec5340fcc54ea754bf9663b3000c6672c
KeizerLI <keizerli1996@163.com>
2018-10-07 22:45:56 +0100

    Scrapy for QQ spider

---
+++ scrapy_qqzp/scrapy_qqzp/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ScrapyQqzpItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+class QQItem(scrapy.Item):
+    '''
+    下面定义需要的数据段
+    '''
+    name = scrapy.Field()
+    detailLink = scrapy.Field()
+    positionInfo = scrapy.Field()
+    workLocation = scrapy.Field()
